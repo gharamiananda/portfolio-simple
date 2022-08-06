ee734a00053919a18d765f27e3c33f4c8a28540d
gharamiananda <anandagharami.am@gmail.com>
2022-08-06 06:29:03 +0100

    content update

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -9,8 +9,8 @@ export default function Intro() {
     init(textRef.current, {
       showCursor: true,
       backDelay: 1500,
-      backSpeed:60,
-      strings: ["Developer", "Designer", "Content Creator"],
+      backSpeed: 60,
+      strings: ["Developer", "Designer", "MERN Developer"],
     });
   }, []);
 
@@ -24,9 +24,9 @@ export default function Intro() {
       <div className="right">
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
-          <h1>Safak Kocaoglu</h1>
+          <h1>Ananda Gharami</h1>
           <h3>
-            Freelance <span ref={textRef}></span>
+            Freelancer <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -11,15 +11,15 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
           </a>
           <div className="itemContainer">
             <Person className="icon" />
-            <span>+44 924 12 74</span>
+            <span>+91 8420377092</span>
           </div>
           <div className="itemContainer">
             <Mail className="icon" />
-            <span>safak@genius.com</span>
+            <span>anandagharami.am@gmail.com</span>
           </div>
         </div>
         <div className="right">
-          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
